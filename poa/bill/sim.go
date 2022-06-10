121dfa6c1e878cfb11ea02f699c6845043d61588
William Kennedy <bill@ardanlabs.com>
2022-06-10 12:54:25 -0400

    staggered start

--- poa/bill/sim.go
+++ poa/bill/sim.go
@@ -43,33 +43,120 @@ func (b block) hash() string {
 
 // =============================================================================
 
-// node represents a node running as a process.
-type node struct {
-	name        string
-	wg          sync.WaitGroup
-	ticker      *time.Ticker
-	shut        chan struct{}
-	send        chan block
-	registry    map[string]*node
-	latestBlock block
+// registry maintains a list of nodes participating in selection.
+type registry struct {
+	name  string
+	mu    sync.RWMutex
+	nodes map[string]*node
 }
 
-// newNode gets the node up and running.
-func newNode(name string) *node {
+// newRegistry constructs a registry for use.
+func newRegistry(n *node) *registry {
+	return &registry{
+		name: n.name,
+		nodes: map[string]*node{
+			n.name: n,
+		},
+	}
+}
+
+// add a new node to the registry.
+func (r *registry) add(node *node) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	r.nodes[node.name] = node
+}
+
+// nodeList returns a slice of the current registry.
+func (r *registry) nodeList() []*node {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	nodes := make([]*node, 0, len(r.nodes))
+	for _, node := range r.nodes {
+		nodes = append(nodes, node)
+	}
+	return nodes
+}
+
+// sendBlock sends the block to all registered nodes.
+func (r *registry) sendBlock(b block) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	for _, node := range r.nodes {
+		if node.name != r.name {
+			node.sendBlock <- b
+		}
+	}
+}
+
+// registerNode sends the node to all registered nodes.
+func (r *registry) registerNode(n *node) {
+	r.mu.RLock()
+	defer r.mu.RUnlock()
+
+	for _, node := range r.nodes {
+		if node.name != r.name {
+			node.registerNode <- n
+		}
+	}
+}
+
+// =============================================================================
+
+// node represents a node running as a process.
+type node struct {
+	name         string
+	latestBlock  block
+	wg           sync.WaitGroup
+	ticker       *time.Ticker
+	shut         chan struct{}
+	registry     *registry
+	sendBlock    chan block
+	registerNode chan *node
+	getNodes     chan chan []*node
+}
+
+// newNode gets the node up and running. The origin node should be the
+// first node we start so other nodes can find other peers.
+func newNode(name string, originNode *node) *node {
 	n := node{
-		name:   name,
-		ticker: time.NewTicker(2 * time.Second),
-		shut:   make(chan struct{}),
-		send:   make(chan block, 10),
+		name:         name,
+		ticker:       time.NewTicker(2 * time.Second),
+		shut:         make(chan struct{}),
+		sendBlock:    make(chan block, 10),
+		registerNode: make(chan *node),
+		getNodes:     make(chan chan []*node),
+	}
+
+	// Register this node in the local registry.
+	n.registry = newRegistry(&n)
+
+	// If this is not the origin node, ask the list of registered
+	// nodes for their registry.
+	if originNode != nil {
+		log.Println(n.name, ":ask", originNode.name, "for registry")
+
+		ch := make(chan []*node)
+		originNode.getNodes <- ch
+		nodes := <-ch
+
+		for _, node := range nodes {
+			log.Println(n.name, ":register", node.name, "on", n.name)
+			n.registry.add(node)
+		}
+
+		// Register this node to the registry of the other nodes.
+		n.registry.registerNode(&n)
 	}
 
 	return &n
 }
 
 // run gets the node up and running.
-func (n *node) run(registry map[string]*node) {
-	n.registry = registry
-
+func (n *node) run() {
 	n.wg.Add(1)
 	go func() {
 		defer n.wg.Done()
@@ -80,9 +167,19 @@ func (n *node) run(registry map[string]*node) {
 			select {
 			case <-n.ticker.C:
 				n.performWork()
-			case b := <-n.send:
+
+			case b := <-n.sendBlock:
 				log.Println(n.name, ":node received block", b.hash())
 				n.latestBlock = b
+
+			case otherNode := <-n.registerNode:
+				log.Println(n.name, ":register node", otherNode.name, "on", n.name)
+				n.registry.add(otherNode)
+
+			case ch := <-n.getNodes:
+				log.Println(n.name, ":send registery")
+				ch <- n.registry.nodeList()
+
 			case <-n.shut:
 				log.Println(n.name, ":node shutting down")
 				return
@@ -111,22 +208,23 @@ func (n *node) performWork() {
 // selection selects an index from 0 to 2.
 func (n *node) selection() string {
 
-	// Sort the current list of registered nodes.
-	nodes := make([]string, 0, len(n.registry))
-	for key := range n.registry {
-		nodes = append(nodes, key)
+	// Sort the current list of registered names.
+	nodes := n.registry.nodeList()
+	names := make([]string, len(nodes))
+	for i, node := range nodes {
+		names[i] = node.name
 	}
-	sort.Strings(nodes)
+	sort.Strings(names)
 
 	// Based on the latest block, pick an index number from
 	// the registry.
 	h := fnv.New32a()
 	h.Write([]byte(n.latestBlock.hash()))
 	integerHash := h.Sum32()
-	i := integerHash % uint32(len(n.registry))
+	i := integerHash % uint32(len(names))
 
 	// Return the name of the node selected.
-	return nodes[i]
+	return names[i]
 }
 
 // mineNewBlock creates a new block and sends that to the p2p network.
@@ -135,11 +233,7 @@ func (n *node) mineNewBlock() {
 	n.latestBlock = b
 
 	// Send block to all other nodes in the registry.
-	for k, node := range n.registry {
-		if k != n.name {
-			node.send <- b
-		}
-	}
+	n.registry.sendBlock(b)
 }
 
 // =============================================================================
@@ -151,18 +245,25 @@ type simulation struct {
 
 // newSimulation starts 3 nodes for the simulation.
 func newSimulation() *simulation {
-	nodes := map[string]*node{
-		"nodeA": newNode("nodeA"),
-		"nodeB": newNode("nodeB"),
-		"nodeC": newNode("nodeC"),
-	}
+	nodeA := newNode("nodeA", nil)
+	nodeA.run()
 
-	for _, n := range nodes {
-		n.run(nodes)
-	}
+	time.Sleep(5 * time.Second)
+
+	nodeB := newNode("nodeB", nodeA)
+	nodeB.run()
+
+	time.Sleep(5 * time.Second)
+
+	nodeC := newNode("nodeC", nodeA)
+	nodeC.run()
 
 	return &simulation{
-		nodes: nodes,
+		nodes: map[string]*node{
+			nodeA.name: nodeA,
+			nodeB.name: nodeB,
+			nodeC.name: nodeC,
+		},
 	}
 }
 
